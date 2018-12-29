124659d5e9bb765942dcd31fa92df558298a7525
V-Legolas <17374501064@163.com>
2018-12-29 19:29:09 +0800

    后台模块开发基本完成，但暂未测试

--- mall-admin/mall-admin-facade/src/main/java/com/mardoner/mall/admin/service/cms/CmsPreferenceAreaService.java
+++ mall-admin/mall-admin-facade/src/main/java/com/mardoner/mall/admin/service/cms/CmsPreferenceAreaService.java
@@ -6,7 +6,7 @@ import com.mardoner.mall.admin.entity.cms.CmsPreferenceArea;
 /**
 * @Description:  优选专区
 * @ClassName: CmsPreferenceAreaService
-* @author whuan-QQ:2500119268
+* @author whuan-QQ:2500129268
 * @email: mardoner12p@gmail.com
 * @date 2018/12/21 15:02
 * @Version 1.0
